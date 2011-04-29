f4743ba09ad4be25c12f4744794a523fdd12aa69
Bess Sadler <bess@stanford.edu>
2011-04-29 15:59:11 -0700

    Adding some example code

--- lib/hydra_fixture_utils.rb
+++ lib/hydra_fixture_utils.rb
@@ -6,6 +6,9 @@ module Hydra
     # Given a foxml fixture object, extract its pid
     # @param [String] file the fully qualified path to the fixture file
     # @return [String] the PID of the object in question
+    # @example
+    #   Hydra::FixtureUtils.extract_pid("/path/to/libra-oa_8.foxml.xml")
+    #   => libra-oa:8
     def self.extract_pid(file)
       raise "Can't find file #{file}" unless File.file? file
       reader = Nokogiri::XML::Reader(IO.read(file))
